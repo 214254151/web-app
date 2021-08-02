18609c314abf33395af6e156e7bc716c08c4e851
217198384 <217198384@mycput.ac.za>
2021-08-02 17:08:18 +0200

    files and components  commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web-app';
+  title = 'Web-App';
+  aField = "Just a field";
+  fieldClass = "basic";
+  condition = false;
+  displaySidebar = false;
+  location: String = "Home";
+  // img src="assets/images/cupcake.jpg"
+  // img src="assets/images/logo2.jpg"   
+
+  public goto(loc:String): void{
+     this.location=loc;
+   
+    // this.aField=(this.location=="home")? "AT HOME" : "FAR AWAY";
+    // this.aField=(this.location=="home")? "green" : "yellow";
+    // this.aField=(this.location=="about")? "blue" : "navy";
+  
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import {MatButtonModule} from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import{MatInputModule} from '@angular/material/input';
+import { FooterComponent } from './footer/footer.component';
+import {MatButtonModule} from '@angular/material/button';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUsComponent,
+    AboutUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    // FooterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
